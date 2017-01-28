8e915319bedf6266ea5373175ccce3a24467e360
Chris Jensen <zigs@zigs.dk>
2017-01-28 20:54:39 +0100

    higher targetworkparts and limit on max creeps

--- ActorRoomUpgrade.js
+++ ActorRoomUpgrade.js
@@ -2,7 +2,8 @@
 
 let ActorWithMemory = require('ActorWithMemory');
 
-const TARGET_WORKPARTS = 10;
+const TARGET_WORKPARTS = 15;
+const MAX_CREEPS = 3;
 const TARGET_RESOURCE_RESERVE = 1500;
 
 module.exports = class ActorRoomUpgrade extends ActorWithMemory
@@ -27,7 +28,7 @@ module.exports = class ActorRoomUpgrade extends ActorWithMemory
 			, energyPos: upgradeContainerPos
 			, controllerId: room.controller.id
 			};
-
+			, creepCount: 0
 	}
 
 	lateInitiate()
@@ -91,24 +92,26 @@ module.exports = class ActorRoomUpgrade extends ActorWithMemory
         this.core.createActor(ACTOR_NAMES.PROCEDUAL_CREEP,
             (script)=>script.initiateActor("upgrader", {workParts: workParts},
             [ [CREEP_INSTRUCTION.SPAWN_UNTIL_SUCCESS,	[spawnId], 						body 					] //0
-            , [CREEP_INSTRUCTION.PICKUP_AT_POS, 		this.memoryObject.energyPos,	RESOURCE_ENERGY,	1000] //1
+            , [CREEP_INSTRUCTION.PICKUP_AT_POS, 		this.memoryObject.energyPos,	RESOURCE_ENERGY,	500 ] //1
             , [CREEP_INSTRUCTION.UPGRADE_UNTIL_EMPTY, 	this.memoryObject.controllerId 							] //2
             , [CREEP_INSTRUCTION.GOTO_IF_ALIVE, 		1 														] //3
             , [CREEP_INSTRUCTION.CALLBACK, 				this.actorId,					"upgraderDied" 			] //4
             , [CREEP_INSTRUCTION.DESTROY_SCRIPT 													  		  ] ] //5
             ));
 
+        this.memoryObject.creepCount++;
 		this.memoryObject.workParts += workParts;
 
-        if(this.memoryObject.workParts < TARGET_WORKPARTS)
+        if(this.memoryObject.workParts < TARGET_WORKPARTS && this.memoryObject.creepCount < MAX_CREEPS)
 			this.requestCreep();
 	}
 
 	upgraderDied(callbackObj)
 	{
+		this.memoryObject.creepCount--;
 		this.memoryObject.workParts -= callbackObj.workParts;
 
-		if(this.memoryObject.workParts < TARGET_WORKPARTS)
+		if(this.memoryObject.workParts < TARGET_WORKPARTS && this.memoryObject.creepCount < MAX_CREEPS)
 			this.requestCreep();
 	}
 };
