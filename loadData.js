dc0e289fbee59b09a7c4d81f7e8b757675182c92
Chris Jensen <zigs@zigs.dk>
2017-09-16 11:22:47 +0200

    order labs in data

--- loadData.js
+++ loadData.js
@@ -109,5 +109,35 @@ module.exports = function()
 			}
 		};
 
-	global.STORAGE_PATTERN = {"name":"textExport","buildings":{"nuker":{"pos":[{"x":0,"y":0}]},"link":{"pos":[{"x":1,"y":0},{"x":0,"y":1}]},"terminal":{"pos":[{"x":2,"y":0}]},"road":{"pos":[{"x":1,"y":1},{"x":2,"y":2},{"x":3,"y":3},{"x":4,"y":4}]},"lab":{"pos":[{"x":2,"y":1},{"x":3,"y":1},{"x":1,"y":2},{"x":3,"y":2},{"x":4,"y":2},{"x":1,"y":3},{"x":2,"y":3},{"x":4,"y":3},{"x":2,"y":4},{"x":3,"y":4}]},"storage":{"pos":[{"x":0,"y":2}]},"powerSpawn":{"pos":[{"x":5,"y":5}]}}};
+	global.STORAGE_PATTERN = {
+		"name":"textExport","buildings":{
+			"nuker":{"pos":[
+				{"x":0,"y":0}]},
+			"link":{"pos":[
+				{"x":1,"y":0},
+				{"x":0,"y":1}]},
+			"terminal":{"pos":[
+				{"x":2,"y":0}]},
+			"road":{"pos":[
+				{"x":4,"y":4},
+				{"x":3,"y":3},
+				{"x":2,"y":2},
+				{"x":1,"y":1}]},
+			"lab":{"pos":[
+				{"x":4,"y":3},
+				{"x":3,"y":4},
+				{"x":4,"y":2},
+				{"x":2,"y":4},
+				{"x":3,"y":2},
+				{"x":2,"y":3},
+				{"x":3,"y":1},
+				{"x":1,"y":3},
+				{"x":2,"y":1},
+				{"x":1,"y":2}]},
+			"storage":{"pos":[
+				{"x":0,"y":2}]},
+			"powerSpawn":{"pos":[
+				{"x":5,"y":5}]}
+		}
+	};
 };
